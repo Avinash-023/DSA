2d540e3588ff8d0c4e64979242d82bffccb74909
AVINASH V <231001023@rajalakshmi.edu.in>
2026-04-12 06:44:25 +0530

    Time: 4 ms (100.00%) | Memory: 47 MB (100.00%) - LeetSync

---
+++ 4280-count-digit-appearances/count-digit-appearances.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countDigitOccurrences(int[] nums, int digit) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            int n=nums[i];
+            while(n>0){
+                int temp=n%10;
+                if(temp==digit) count++;
+                n/=10;
+            }
+        }
+        return count;
+    }
+}
