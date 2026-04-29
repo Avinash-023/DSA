77fe870c200c25b043549f56a670c03369e39050
AVINASH V <231001023@rajalakshmi.edu.in>
2026-04-29 06:50:37 +0530

    Time: 3 ms (18.42%) | Memory: 43.1 MB (32.84%) - LeetSync

---
+++ 1434-decrypt-string-from-alphabet-to-integer-mapping/decrypt-string-from-alphabet-to-integer-mapping.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String freqAlphabets(String s) {
+        List<Integer> arr=new ArrayList<>();
+        String alpha="abcdefghijklmnopqrstuvwxyz";
+        StringBuilder sb=new StringBuilder();
+        for(int right=s.length()-1;right>=0;right--){
+            int n=s.charAt(right)-'0';
+            if(s.charAt(right)=='#'){
+                String str=s.substring(right-2,right);
+                n=Integer.parseInt(str);
+                right-=2;
+            }
+            arr.add(n);
+            sb.insert(0,alpha.charAt(n-1));
+        }
+        // System.out.println(arr);
+        return sb.toString();
+    }
+}
